2fcb96dd47308dcb6d2d041513d637f670a83282
Reilly00 <114668961+Reilly00@users.noreply.github.com>
2023-11-10 11:54:06 +0000

    Updated build.gradle.kts
    Updated build.grade.kts to compileSDK at 34 instead of 33. This fixes an error when trying to run the base code.

--- Mobile Integration/app/build.gradle.kts
+++ Mobile Integration/app/build.gradle.kts
@@ -5,12 +5,13 @@ plugins {
 
 android {
     namespace = "com.example.careconnect"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.careconnect"
         minSdk = 24
-        targetSdk = 33
+        //noinspection EditedTargetSdkVersion
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
