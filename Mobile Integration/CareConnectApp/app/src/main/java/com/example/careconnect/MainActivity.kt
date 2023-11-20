1914359b6602a202eaf2b6bc88f705b85623ae7b
Matthew-Riddell <d00245674@student.dkit.ie>
2023-11-20 10:08:52 +0000

    Skeleton Code for Dashboard

--- Mobile Integration/CareConnectApp/app/src/main/java/com/example/careconnect/MainActivity.kt
+++ Mobile Integration/CareConnectApp/app/src/main/java/com/example/careconnect/MainActivity.kt
@@ -3,13 +3,27 @@ package com.example.careconnect
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.careconnect.ui.theme.CareConnectTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Dashboard()
                 }
             }
         }
@@ -30,17 +44,70 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Dashboard() {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        item {
+            Text(
+                text = "Dashboard",
+                style = TextStyle.Default,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+        }
+
+        items(getDashboardItems()) { dashboardItem ->
+            DashboardItem(dashboardItem)
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DashboardItem(item: DashboardItemModel) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 8.dp),
+        shape = MaterialTheme.shapes.medium,
+        onClick = { /* Handle item click here */ }
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = item.title,
+                style = TextStyle.Default,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+
+            Text(
+                text = item.subtitle,
+                style = TextStyle.Default, // Use bodyMain instead of body1
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+        }
+    }
+}
+
+data class DashboardItemModel(val title: String, val subtitle: String)
+
+fun getDashboardItems(): List<DashboardItemModel> {
+    return listOf(
+        DashboardItemModel("Item 1", "Subtitle for Item 1"),
+        DashboardItemModel("Item 2", "Subtitle for Item 2"),
+        DashboardItemModel("Item 3", "Subtitle for Item 3"),
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DashboardPreview() {
     CareConnectTheme {
-        Greeting("Android")
+        Dashboard()
     }
 }
