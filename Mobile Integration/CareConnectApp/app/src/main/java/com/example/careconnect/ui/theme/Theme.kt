a450368e57b2cfb8b20f9dcc36e48a7d6b809dd1
Reilly00 <114668961+Reilly00@users.noreply.github.com>
2023-11-21 10:20:27 +0000

    Modified the Theme
    Changed the colours in the color file to reflect those changes in the theme file. Making the app more relevant to our colour scheme.

--- Mobile Integration/CareConnectApp/app/src/main/java/com/example/careconnect/ui/theme/Theme.kt
+++ Mobile Integration/CareConnectApp/app/src/main/java/com/example/careconnect/ui/theme/Theme.kt
@@ -15,32 +15,21 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+private val LightColorScheme = lightColorScheme(
+    primary = LightPurple,
+    secondary = SoftBlue,
+    tertiary = White
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val DarkColorScheme = darkColorScheme(
+    primary = SoftBlue,
+    secondary = DarkGray,
+    tertiary = LightPurple
 )
 
 @Composable
 fun CareConnectTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -49,7 +38,6 @@ fun CareConnectTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
@@ -63,7 +51,7 @@ fun CareConnectTheme(
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = LightColorScheme,
         typography = Typography,
         content = content
     )
