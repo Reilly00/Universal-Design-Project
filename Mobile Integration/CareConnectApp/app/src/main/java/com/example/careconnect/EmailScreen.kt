1be5ba40025af17f44fb3f1eec6b8301006a9b80
Matthew-Riddell <d00245674@student.dkit.ie>
2023-12-07 18:09:46 +0000

    Update EmailScreen.kt

--- Mobile Integration/CareConnectApp/app/src/main/java/com/example/careconnect/EmailScreen.kt
+++ Mobile Integration/CareConnectApp/app/src/main/java/com/example/careconnect/EmailScreen.kt
@@ -1,4 +1,43 @@
 package com.example.careconnect
 
-class EmailScreen {
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.careconnect.ui.theme.CareConnectTheme
+
+@Composable
+fun EmailScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+
+        BottomNavigationBar()
+    }
 }
