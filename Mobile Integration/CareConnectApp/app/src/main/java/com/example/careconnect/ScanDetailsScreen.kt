a609e90adc2b49fc4cafa6c2379848a9c7274feb
Matthew-Riddell <d00245674@student.dkit.ie>
2023-12-07 16:45:59 +0000

    Update ScanDetailsScreen.kt

--- Mobile Integration/CareConnectApp/app/src/main/java/com/example/careconnect/ScanDetailsScreen.kt
+++ Mobile Integration/CareConnectApp/app/src/main/java/com/example/careconnect/ScanDetailsScreen.kt
@@ -1,4 +1,32 @@
 package com.example.careconnect
 
-class ScanDetailsScreen {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.careconnect.ui.theme.CareConnectTheme
+
+@Composable
+fun ScanDetailsScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        BottomNavigationBar()
+    }
 }
