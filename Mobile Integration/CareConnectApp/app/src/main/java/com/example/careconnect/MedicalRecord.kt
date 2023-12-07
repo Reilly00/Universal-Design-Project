a67fcfdce99250e0634dd8605d9c99a32dd07e36
Matthew-Riddell <d00245674@student.dkit.ie>
2023-12-07 19:41:47 +0000

    Unfinished Update Record Page
    Requires input functionality

--- Mobile Integration/CareConnectApp/app/src/main/java/com/example/careconnect/MedicalRecord.kt
+++ Mobile Integration/CareConnectApp/app/src/main/java/com/example/careconnect/MedicalRecord.kt
@@ -1,16 +1,20 @@
 package com.example.careconnect
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 
 @Composable
-fun MedicalRecord(record: RecordModel) {
+fun MedicalRecord(record: RecordModel, navController: NavController) {
     Column(
         modifier = Modifier.padding(16.dp)
     ) {
@@ -34,6 +38,20 @@ fun MedicalRecord(record: RecordModel) {
             color = Color.Black,
             modifier = Modifier.padding(bottom = 8.dp)
         )
+
+        // "Update Record" button
+        Button(
+            onClick = {
+                // Navigate to UpdateMedicalRecordActivity with the current record details
+                navController.navigate("UpdateMedicalRecord/${record.id}")
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.primary)
+                .padding(top = 16.dp)
+        ) {
+            Text("Update Record")
+        }
 
         // Bottom navigation bar at the end of the Column
         BottomNavigationBar()
