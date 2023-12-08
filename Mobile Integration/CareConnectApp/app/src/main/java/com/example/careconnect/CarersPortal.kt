a89c38eaaee97fc3d99e5d0d5fd4abaef9147bde
Erlinguss <D00251914@student.dkit.ie>
2023-12-08 23:19:24 +0000

    Added a box, name and hour in care's portal class

--- Mobile Integration/CareConnectApp/app/src/main/java/com/example/careconnect/CarersPortal.kt
+++ Mobile Integration/CareConnectApp/app/src/main/java/com/example/careconnect/CarersPortal.kt
@@ -1,10 +1,15 @@
 package com.example.careconnect
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.KeyboardOptions.Companion.Default
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Send
@@ -14,26 +19,32 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.SemanticsProperties.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.careconnect.ui.theme.CareConnectTheme
+import androidx.compose.material.icons.filled.Send
 
 data class MessageModel(val sender: String, val content: String)
 
 @Composable
-fun CarersPortal(navController: NavController? = null, loggedInUser: User) {
+fun CarersPortal(navController: NavController? = null) {
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
-        MessagingContent(loggedInUser)
+        MessagingContent()
         navController?.let { BottomNavigationBar(it) }
     }
 }
 
-
 @Composable
-fun MessagingContent(loggedInUser: User) {
+fun MessagingContent() {
     var message by remember { mutableStateOf("") }
     var messages by remember { mutableStateOf(listOf<MessageModel>()) }
 
@@ -46,24 +57,57 @@ fun MessagingContent(loggedInUser: User) {
         MessageList(messages = messages)
 
         // Input for typing messages
-        MessageInput(loggedInUser = loggedInUser, onSendMessage = { sender, newMessage ->
-            messages = messages + MessageModel(sender.name, newMessage)
+        MessageInput(onSendMessage = { sender, newMessage ->
+            messages = messages + MessageModel(sender, newMessage)
         })
     }
 }
-
 @Composable
 fun MessageList(messages: List<MessageModel>) {
     LazyColumn {
         items(messages) { message ->
-            // Display each message
-            Text(
-                text = "${message.sender}: ${message.content}",
-                style = MaterialTheme.typography.titleSmall
-            )
+            // Display each message in a designed layout
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp)
+            ) {
+                // Message content in a light pink box
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                        .background(color = Color(0xFFF1E0E6), shape = MaterialTheme.shapes.medium)
+                        .padding(16.dp)
+                ) {
+                    Text(
+                        text = message.content,
+                        color = Color.Black
+                    )
+                }
+
+                // Sender and timestamp outside the box
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp, start = 16.dp, end = 16.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = message.sender,
+                        color = Color.Gray,
+                    )
+                    Text(
+                        text = "Timestamp", // Replace with actual timestamp logic
+                        color = Color.Gray,
+                    )
+                }
+            }
         }
     }
 }
+
+
 
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
 @Composable
