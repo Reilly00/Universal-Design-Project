b0d63ccbdd4f4cc53d76564270db729e51706d5a
Reilly00 <114668961+Reilly00@users.noreply.github.com>
2023-12-10 23:44:57 +0000

    Working Login via Retrofit

--- Mobile Integration/CareConnectApp/app/src/main/java/com/example/careconnect/LoginScreen.kt
+++ Mobile Integration/CareConnectApp/app/src/main/java/com/example/careconnect/LoginScreen.kt
@@ -2,28 +2,25 @@ package com.example.careconnect
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen(navController: NavController? = null) {
     var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    var loginStatus by remember { mutableStateOf(LoginStatus.NONE) }
+    val coroutineScope = rememberCoroutineScope()
 
     Column(
         modifier = Modifier
@@ -55,21 +52,41 @@ fun LoginScreen(navController: NavController? = null) {
         )
         Spacer(modifier = Modifier.height(16.dp))
         Button(
-            onClick = { /* TODO: Add login logic here, not needed for current build */ },
+            onClick = {
+                coroutineScope.launch {
+                    val response = RetrofitClient.instance.loginUser(LoginData(username, password))
+                    loginStatus = if (response.isSuccessful) {
+                        LoginStatus.SUCCESS
+                    } else {
+                        LoginStatus.ERROR
+                    }
+                }
+            },
             shape = MaterialTheme.shapes.medium,
             modifier = Modifier.fillMaxWidth(fraction = 0.5f),
             contentPadding = PaddingValues(16.dp)
         ) {
-            Text(
-                text = "Login",
-                modifier = Modifier.clickable { navController?.navigate("dashboard") }
-            )
+            Text(text = "Login")
         }
+
+        when (loginStatus) {
+            LoginStatus.SUCCESS -> {
+                Text("Login successful")
+                // Optionally navigate to dashboard or another screen
+            }
+            LoginStatus.ERROR -> Text("Login failed", color = Color.Red)
+            else -> {} // Do nothing for NONE
+        }
+
         Spacer(modifier = Modifier.height(10.dp))
         Text(
             text = "Don't have an account? Register here",
             modifier = Modifier.clickable { navController?.navigate("register") }
         )
-        Spacer(modifier = Modifier.weight(1f)) // Flexible spacer to balance the content
+        Spacer(modifier = Modifier.weight(1f))
     }
+}
+
+enum class LoginStatus {
+    NONE, SUCCESS, ERROR
 }
