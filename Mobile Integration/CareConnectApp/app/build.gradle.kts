11ec8d1beb8b9853d65b18c3060900114c82b829
Abiel <97120759+abielinsky@users.noreply.github.com>
2023-12-12 02:23:50 +0000

    implementations pubnub
    implementations pubnub

--- Mobile Integration/CareConnectApp/app/build.gradle.kts
+++ Mobile Integration/CareConnectApp/app/build.gradle.kts
@@ -65,6 +65,40 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
 
+
+
+    implementation("com.pubnub:pubnub-android:6.0.0")
+    implementation("com.pubnub:pubnub-gson:6.0.0")
+    implementation("com.pubnub:pubnub-okhttp:6.0.0")
+    implementation("com.pubnub:pubnub-logging:6.0.0")
+    implementation("com.pubnub:pubnub-crypto:6.0.0")
+    implementation("com.pubnub:pubnub:5.9.0")
+    implementation("com.pubnub:pubnub-gson:5.9.0")
+
+    implementation ("com.google.android.material:material3:1.5.0-alpha01")
+    implementation ("androidx.compose.material3:material-icons-extended:1.0.0-beta02")
+    implementation ("androidx.activity:activity-compose:1.3.1")
+    implementation ("com.pubnub:pubnub-gson:4.40.5")
+    implementation ("com.pubnub:pubnub-android:7.0.0")
+
+    implementation ("io.github.franmcod:pubnub-compose:1.0.0") // Add the PubNub Compose library
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
+    implementation ("androidx.activity:activity-compose:1.3.1")
+    implementation ("androidx.compose.ui:ui-tooling:1.0.5")
+    implementation ("androidx.navigation:navigation-compose:2.4.0-alpha02")
+    implementation ("androidx.compose.material:material:1.0.5")
+
+
+
+
+
+
+
+
+
+
+
     implementation("androidx.compose.ui:ui-text-android:1.5.4")
     implementation("androidx.camera:camera-view:1.3.0")
     testImplementation("junit:junit:4.13.2")
@@ -81,4 +115,7 @@ dependencies {
     implementation ("androidx.camera:camera-extensions:1.0.0-alpha30")
     implementation ("androidx.camera:camera-lifecycle:1.1.0")
     //implementation("com.journeyapps:zxing-android-embedded:4.2.0")
+
+
+
 }
