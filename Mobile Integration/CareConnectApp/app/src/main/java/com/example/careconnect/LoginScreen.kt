c89df66b41e18141918a32182486de101b61145a
Erlinguss <D00251914@student.dkit.ie>
2023-12-17 17:55:52 +0000

    Merge branch 'design1' of C:\Users\User\AndroidStudioProjects\Mobile App\Universal-Design-Project with conflicts.

--- Mobile Integration/CareConnectApp/app/src/main/java/com/example/careconnect/LoginScreen.kt
+++ Mobile Integration/CareConnectApp/app/src/main/java/com/example/careconnect/LoginScreen.kt
@@ -1,11 +1,8 @@
 package com.example.careconnect
 
-import UserViewModel
-import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
